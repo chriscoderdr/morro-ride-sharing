2d067983333071da356c15c08ba9a9032c5555ae
Chris Gomez <christiangomezdev@gmail.com>
2024-11-02 12:38:27 +0800

    feat(#29): add notifications for new ride requests (#30)
    - Implemented notification permissions request for driver
    - Displayed token for testing notification permissions
    - Added new MQTT topic for ride request handling
    - Introduced ride request reducer in Redux for state management

--- apps/driver-app/src/services/mqtt-client-service.ts
+++ apps/driver-app/src/services/mqtt-client-service.ts
@@ -1,12 +1,17 @@
 import {
   MQTT_BROKER_URL,
   MQTT_PORT,
-  MQTT_TOPIC
+  MQTT_TOPIC,
+  MQTT_TOPIC_RIDE_REQUESTS
 } from '@/src/config/mqtt-config';
+import * as Notifications from 'expo-notifications';
 import { Client, Message } from 'paho-mqtt';
+import store from '../store';
+import { setRideRequest } from '../store/slices/ride-request-slice';
 
 class MQTTClientService {
   private client: Client;
+  private isSuscribed: boolean = false;
 
   constructor(clientId: string) {
     console.log(`connection info ${MQTT_BROKER_URL} ${MQTT_PORT} ${clientId}`);
@@ -31,6 +36,24 @@ class MQTTClientService {
     });
   };
 
+  subscribeToRideRequests = (accessToken: string = '') => {
+    if (!this.isSuscribed && accessToken.length > 0) {
+      const topic = MQTT_TOPIC_RIDE_REQUESTS.replaceAll(
+        '${driver_id}',
+        accessToken
+      );
+      this.isSuscribed = true;
+      this.client.subscribe(topic, {
+        onSuccess: () => {
+          console.log('Subscribed to MQTT topic:', topic);
+        },
+        onFailure: (error: any) => {
+          console.error('Failed to subscribe to MQTT topic:', error);
+        }
+      });
+    }
+  };
+
   onConnectionLost = (responseObject: {
     errorCode: number;
     errorMessage?: string;
@@ -42,6 +65,49 @@ class MQTTClientService {
 
   onMessageArrived = (message: Message) => {
     console.log('Message received:', message.payloadString);
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false
+      })
+    });
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'You got a new ride requests'
+        // sound: 'mySoundFile.wav', // Provide ONLY the base filename
+      },
+      trigger: {
+        seconds: 2,
+        channelId: 'new-ride-request'
+      }
+    })
+      .then((notificationId) => {
+        console.log(notificationId);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    // store.dispatch(clearRideRequest());
+    const rideRequest = JSON.parse(message.payloadString);
+    store.dispatch(
+      setRideRequest({
+        rideRequestId: rideRequest.rideRequestId,
+        estimatedPrice: rideRequest.estimatedPrice,
+        pickupTimeDistance: rideRequest.pickupTimeDistance,
+        pickupLocation: {
+          latitude: rideRequest.pickupLocation.latitude,
+          longitude: 121.1301362,
+          address: rideRequest.pickupLocation.address
+        },
+        tripTimeDistance: rideRequest.tripTimeDistance,
+        tripLocation: {
+          latitude: rideRequest.tripLocation.latitude,
+          longitude: rideRequest.tripLocation.longitude,
+          address: rideRequest.tripLocation.address
+        }
+      })
+    );
   };
 
   publishLocation = (
@@ -50,7 +116,10 @@ class MQTTClientService {
     accessToken: string
   ) => {
     if (this.client.isConnected() && accessToken.length > 0) {
-      console.log(`publishing location ${latitude} ${longitude} ${accessToken}`);
+      this.subscribeToRideRequests(accessToken);
+      console.log(
+        `publishing location ${latitude} ${longitude} ${accessToken}`
+      );
       const payload = JSON.stringify({
         latitude,
         longitude,
