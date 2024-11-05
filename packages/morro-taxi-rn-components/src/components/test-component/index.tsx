06cd7413b78fe77a1999a92981abcf42d4764193
Chris Gomez <christiangomezdev@gmail.com>
2024-11-06 07:04:20 +0800

    feat(#49): User Story: As a new rider, I want to provide my personal information to create an account.
    - fixes regarding https://github.com/facebook/react/issues/24928

---
+++ packages/morro-taxi-rn-components/src/components/test-component/index.tsx
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+
+const TestComponent = () => {
+    return (<Text>Testing Something</Text>)
+}
+export default TestComponent;
--- packages/morro-taxi-rn-components/src/index.tsx
+++ packages/morro-taxi-rn-components/src/index.tsx
@@ -1,3 +1,7 @@
+import TestComponent from "./components/test-component";
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export { TestComponent };
