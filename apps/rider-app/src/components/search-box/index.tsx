8b506bc601add5afcd4ce18670efc00d74ae105b
Chris Gomez <christiangomezdev@gmail.com>
2024-11-07 05:59:52 +0800

    feat(#51): As a rider I want to request a ride so that I can get to my destination
    - implmenting basic search with search input and mapbox

--- apps/rider-app/src/components/search-box/index.tsx
+++ apps/rider-app/src/components/search-box/index.tsx
@@ -1,24 +1,50 @@
 import { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import {
   InputText,
   InputTextField
 } from 'react-native-morro-taxi-rn-components';
+import { SearchBoxCore, SessionToken } from '@mapbox/search-js-core';
+import config from '@/src/config';
 
 export const SearchBox = () => {
-    const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [places, setPlaces] = useState([]);
 
-    const handleSearchQueryChange = (text: string) => {
-        setSearchQuery(text);
-    };
+  const searchPlace = async (query: string) => {
+    const search = new SearchBoxCore({
+      accessToken: config.MAPBOX_ACCESS_TOKEN
+    });
+    const sessionToken = new SessionToken();
+    const result = await search.suggest(query, { sessionToken });
+    setPlaces(result.suggestions);
+    console.log(`Search result: ${JSON.stringify(result)}`);
+  };
 
-    useEffect(() => {
-        console.log(searchQuery);
-    }, [searchQuery]);
+  useEffect(() => {
+    if (searchQuery.length > 3) {
+      searchPlace(searchQuery);
+    }
+  }, [searchQuery]);
+
+  const handleSearchQueryChange = (text: string) => {
+    setSearchQuery(text);
+  };
+
+  useEffect(() => {
+    console.log(searchQuery);
+  }, [searchQuery]);
 
   return (
     <View style={{ paddingVertical: 100, paddingHorizontal: 20 }}>
-      <InputText placeholder="Where to?" onChangeText={handleSearchQueryChange} />
+      <InputText
+        placeholder="Where to?"
+        onChangeText={handleSearchQueryChange}
+      />
+      <FlatList
+        data={places}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 };
